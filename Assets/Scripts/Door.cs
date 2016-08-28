ed7a1e6db825c203ef73b921935575ae4fcbef0e
silent-hero <p.jazauskas@gmail.com>
2016-08-28 21:04:29 +0300

    doors have arrow

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -5,9 +5,11 @@ public class Door : MonoBehaviour
 {
     public string objectRequired = "p2";
     public GameObject keyHole;
+    public GameObject arrow;
     public float animDelay = 3;
     public string firstAnim;
     public Collider doorCollider;
+
 
     private bool opened = false;
 
@@ -30,6 +32,7 @@ public class Door : MonoBehaviour
                 other.GetComponent<PickItem>().followSpeed = 1;
                 other.GetComponent<PickItem>().objectToFollow = keyHole;
                 other.GetComponent<PickItem>().openLater(this.gameObject, firstAnim, animDelay);
+                Destroy(arrow);
             }
         }
     }
