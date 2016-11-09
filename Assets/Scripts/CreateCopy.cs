787b5968561439b9291f4376287e2da959d3c21d
silent-hero <p.jazauskas@gmail.com>
2016-11-09 18:53:18 +0200

    finished copyCreator

--- Assets/Scripts/CreateCopy.cs
+++ Assets/Scripts/CreateCopy.cs
@@ -4,35 +4,73 @@ using System.Collections.Generic;
 
 
 public class CreateCopy : MonoBehaviour {
-    public string name = "Roomc";
-    public string shortCode = "rd";
-    private string[,] codes = { { "16", "32", "64", "128" }, { "48", "96", "144", "192" }, { "240", "0", "0", "0" }, { "112", "176", "208", "224" }, { "80", "160", "0", "0" }, { "52", "104", "146", "193" }, { "18", "36", "72", "129" }, { "22", "44", "73", "131" }, { "20", "40", "65", "130" } };
-    public List<string> names;
-    private int[] timesCount = { 4, 4, 1, 4, 2, 4, 4, 4, 4 };
-    public List<GameObject> objects;
+    public string roomName = "Roomc";
+    public string roomShortCode = "rd";
+    private string[,] roomCodes = { { "16", "32", "64", "128" }, { "48", "96", "144", "192" }, { "240", "0", "0", "0" }, { "112", "176", "208", "224" }, { "80", "160", "0", "0" }, { "52", "104", "146", "193" }, { "18", "36", "72", "129" }, { "22", "44", "73", "131" }, { "20", "40", "65", "130" } };
+    public List<string> roomNames;
+    private int[] roomTimesCount = { 4, 4, 1, 4, 2, 4, 4, 4, 4 };
+    public List<GameObject> roomObjects;
+    public string roomChildName = "Tunnel1";
     [System.NonSerialized]
     private int[,] roomAngles = { { 270, 0, 90, 180 }, { 0, 90, 270, 180 }, { 0, 0, 0, 0 }, { 0, 270, 180, 90 }, { 90, 0, 0, 0 }, { 0, 90, 270, 180 }, { 270, 0, 90, 180 }, { 270, 0, 90, 180 }, { 270, 0, 90, 180 }};
-	// Use this for initialization
+
+    public string tunnelName = "Tunnelc";
+    public string tunnelShortCode = "tc";
+    private string[,] tunnelCodes = { { "1", "2", "4", "8" }, { "5", "10", "0", "0" }, { "7", "11", "13", "14" }, { "3", "6", "9", "12" }, { "15", "0", "0", "0" } };
+    public List<string> tunnelNames;
+    private int[] tunnelTimesCount = { 4, 2, 4, 4, 1};
+    public List<GameObject> tunnelObjects;
+    public string tunnelChildName = "Tunnel1";
+    private int[,] tunnelAngles = { { 180, 270, 0, 90 }, { 0, 90, 0, 0 }, { 90, 0, 270, 180 }, { 0, 90, 270, 180 }, { 0, 0, 0, 0 } };
+
 	void Start ()
     {
         GameObject cloneObject = transform.gameObject;
 
         for (var a = 1; a < 10; a++ )
         {
-            names.Add(name + (a));
+            roomNames.Add(roomName + (a));
         }
 
-        for (var b = 0; b < objects.Count; b++)
+        for (var a = 1; a < 6; a++)
         {
-            for (var a = 0; a < timesCount[b]; a++)
+            tunnelNames.Add(tunnelName + (a));
+        }
+
+        for (var b = 0; b < roomObjects.Count; b++)
+        {
+            for (var a = 0; a < roomTimesCount[b]; a++)
             {
-                GameObject a2 = Instantiate(objects[b], transform.position, Quaternion.Euler(0f, roomAngles[b, a], 0f)) as GameObject;
-                a2.GetComponent<Model>().code = shortCode + codes[b, a];
-                a2.GetComponent<Model>().name = name;
+                GameObject a2 = Instantiate(roomObjects[b], transform.position, Quaternion.Euler(0f, roomAngles[b, a], 0f)) as GameObject;
+                a2.GetComponent<Model>().code = roomShortCode + roomCodes[b, a];
+                a2.GetComponent<Model>().name = roomName;
                 a2.tag = "Room";
+                a2.isStatic = true;
+                a2.transform.FindChild(roomChildName).GetComponent<MeshRenderer>().receiveShadows = false;
+                a2.transform.FindChild(roomChildName).GetComponent<Renderer>().shadowCastingMode = 0;
+                a2.transform.FindChild(roomChildName).gameObject.isStatic = false;
                 a2.transform.position = new Vector3(0f, 0f, 0f);
                 if (a2.GetComponent<CreateCopy>()) Destroy(a2.GetComponent<CreateCopy>());
-                a2.name = names[b] + " (" + shortCode + codes[b, a] + ")";
+                a2.name = roomNames[b] + " (" + roomShortCode + roomCodes[b, a] + ")";
+                cloneObject = a2;
+            }
+        }
+
+        for (var b = 0; b < tunnelObjects.Count; b++)
+        {
+            for (var a = 0; a < tunnelTimesCount[b]; a++)
+            {
+                GameObject a2 = Instantiate(tunnelObjects[b], transform.position, Quaternion.Euler(0f, tunnelAngles[b, a], 0f)) as GameObject;
+                a2.GetComponent<Model>().code = tunnelShortCode + tunnelCodes[b, a];
+                a2.GetComponent<Model>().name = tunnelName;
+                a2.tag = "Tunnel";
+                a2.transform.position = new Vector3(0f, 0f, 0f);
+                a2.isStatic = true;
+                a2.transform.FindChild(tunnelChildName).GetComponent<MeshRenderer>().receiveShadows = false;
+                a2.transform.FindChild(tunnelChildName).GetComponent<Renderer>().shadowCastingMode = 0;
+                a2.transform.FindChild(tunnelChildName).gameObject.isStatic = true;
+                if (a2.GetComponent<CreateCopy>()) Destroy(a2.GetComponent<CreateCopy>());
+                a2.name = tunnelNames[b] + " (" + tunnelShortCode + tunnelCodes[b, a] + ")";
                 cloneObject = a2;
             }
         }
